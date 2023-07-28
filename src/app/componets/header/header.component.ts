0a4e6d46e751f8ceaa8dc7bd39a2a8cbe2412877
Alex <alexgouveiadearaujo@gmail.com>
2023-07-28 20:57:21 -0300

    create headerComponet

---
+++ src/app/componets/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Router } from '@angular/router';
+import { UserService } from './../../authentication/user/user.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  user$ = this.userService.returnUser();
+  constructor(private userService: UserService, private router: Router) {console.log('first' , this.user$)}
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
+}
