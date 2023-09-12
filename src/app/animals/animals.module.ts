005189a079e6bee6b40ccf89fe02f6120b92ffd0
Alex <alexgouveiadearaujo@gmail.com>
2023-09-12 15:46:50 -0300

    create animals list

--- src/app/animals/animals.module.ts
+++ src/app/animals/animals.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AnimalsRoutingModule } from './animals-routing.module';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
+import { AnimalComponent } from './animal/animal.component';
+import { AnimalPhotoGridComponent } from './animal-photo-grid/animal-photo-grid.component';
+import { CardModule } from '../componets/card/card.module';
 
 
 @NgModule({
   declarations: [
-    ListAnimalsComponent
+    ListAnimalsComponent,
+    AnimalComponent,
+    AnimalPhotoGridComponent
   ],
   imports: [
     CommonModule,
-    AnimalsRoutingModule
+    AnimalsRoutingModule,
+    CardModule
   ]
 })
 export class AnimalsModule { }
--- src/app/animals/list-animals/list-animals.component.ts
+++ src/app/animals/list-animals/list-animals.component.ts
@@ -1,15 +1,33 @@
+import { AnimalsService } from './../animals.service';
 import { Component, OnInit } from '@angular/core';
+import { Ianimals } from '../ianimal';
+import { UserService } from 'src/app/authentication/user/user.service';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-list-animals',
   templateUrl: './list-animals.component.html',
-  styleUrls: ['./list-animals.component.css']
+  styleUrls: ['./list-animals.component.css'],
 })
 export class ListAnimalsComponent implements OnInit {
-
-  constructor() { }
+  animals$!: Observable<Ianimals>;
+  constructor(
+    private userService: UserService,
+    private animalsService: AnimalsService
+  ) {}
 
   ngOnInit(): void {
+    // this.userService.returnUser().subscribe((user)=>{
+    //   const userName = user.name ?? '';
+    //   this.animalsService.userList(userName).subscribe((animals)=>{
+    //     this.animals = animals;
+    //   })
+    // })
+    this.animals$ = this.userService.returnUser().pipe(
+      switchMap((user) => {
+        const userName = user.name ?? '';
+        return this.animalsService.userList(userName);
+      })
+    );
   }
-
 }
